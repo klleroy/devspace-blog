ac4f4652812508db7a853b2c5f6900e27e3cff7a
Kevin LeRoy <kevin@kevinleroy.me>
2021-07-22 17:11:15 -0700

    Cleaned up posts & created Category Sidebar.

--- pages/blog/page/[page_index].js
+++ pages/blog/page/[page_index].js
@@ -1,24 +1,30 @@
 import fs from 'fs';
 import path from 'path';
-import Link from 'next/link';
-import matter from 'gray-matter';
-import Layout from '../../../components/Layout';
-import Post from '../../../components/Post';
-import Pagination from '../../../components/Pagination';
-import { sortByDate } from '../../../utils';
-import { POSTS_PER_PAGE } from '../../../config';
+import Layout from '@/components/Layout';
+import Post from '@/components/Post';
+import Pagination from '@/components/Pagination';
+import CategoryList from '@/components/CategoryList';
+import { POSTS_PER_PAGE } from '@/config/index';
+import { getPosts } from '@/lib/posts';
 
-export default function BlogPage({ posts, numPages, currentPage }) {
+export default function BlogPage({ posts, numPages, currentPage, categories }) {
 	return (
 		<Layout>
-			<h1 className='text-5xl border-b-4 p-5 font-bold'>Blog</h1>
+			<div className='flex justify-between'>
+				<div className='w-3/4 mr-10'>
+					<h1 className='text-5xl border-b-4 p-5 font-bold'>Blog</h1>
 
-			<div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
-				{posts.map((post, index) => (
-					<Post key={index} post={post} />
-				))}
+					<div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+						{posts.map((post, index) => (
+							<Post key={index} post={post} />
+						))}
+					</div>
+					<Pagination currentPage={currentPage} numPages={numPages} />
+				</div>
+				<div className='w-1/4'>
+					<CategoryList categories={categories} />
+				</div>
 			</div>
-			<Pagination currentPage={currentPage} numPages={numPages} />
 		</Layout>
 	);
 }
@@ -47,33 +53,25 @@ export async function getStaticProps({ params }) {
 
 	const files = fs.readdirSync(path.join('posts'));
 
-	const posts = files.map((filename) => {
-		const slug = filename.replace('.md', '');
+	const posts = getPosts();
 
-		const markdownWithMeta = fs.readFileSync(
-			path.join('posts', filename),
-			'utf-8'
-		);
-
-		const { data: frontmatter } = matter(markdownWithMeta);
-
-		return {
-			slug,
-			frontmatter,
-		};
-	});
+	// Get categories for sidebar
+	const categories = posts.map((post) => post.frontmatter.category);
+	const uniqueCategories = [...new Set(categories)];
 
 	const numPages = Math.ceil(files.length / POSTS_PER_PAGE);
 	const pageIndex = page - 1;
-	const orderedPosts = posts
-		.sort(sortByDate)
-		.slice(pageIndex * POSTS_PER_PAGE, (pageIndex + 1) * POSTS_PER_PAGE);
+	const orderedPosts = posts.slice(
+		pageIndex * POSTS_PER_PAGE,
+		(pageIndex + 1) * POSTS_PER_PAGE
+	);
 
 	return {
 		props: {
 			posts: orderedPosts,
 			numPages,
 			currentPage: page,
+			categories: uniqueCategories,
 		},
 	};
 }
